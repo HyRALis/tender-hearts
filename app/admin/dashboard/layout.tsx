0092f48b85dee0a769b1468f43d1247041dd4c2b
HyRALis <petar.trajanoski.pt@gmail.com>
2024-05-28 18:08:43 +0200

    formating and removed use client

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -1,4 +1,3 @@
-'use client';
 import GenericDashboardLayout from '@/app/Shared/GenericDashboardLayout';
 import { TListItemType } from '@/app/Shared/components/Sidebar/primitives/SidebarListItem';
 import { Inter } from 'next/font/google';
@@ -6,19 +5,25 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function AdminDashboardLayout({
-    children
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    const sidebarItems: TListItemType[] = ['Dashboard', 'Manage Users', 'Manage Requests', 'Reports', 'Logout'];
+  const sidebarItems: TListItemType[] = [
+    'Dashboard',
+    'Manage Users',
+    'Manage Requests',
+    'Reports',
+    'Logout',
+  ];
 
-    return (
-      <GenericDashboardLayout
-        sidebarItems={sidebarItems}
-        pageName='Admin'
-        portal={'admin'}
-      >
-        {children}
-      </GenericDashboardLayout>
-    );
+  return (
+    <GenericDashboardLayout
+      sidebarItems={sidebarItems}
+      pageName='Admin'
+      portal={'admin'}
+    >
+      {children}
+    </GenericDashboardLayout>
+  );
 }
