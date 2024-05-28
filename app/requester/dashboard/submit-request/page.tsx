b31001b931725215270825afa12e52c8ed454204
HyRALis <petar.trajanoski.pt@gmail.com>
2024-05-28 16:35:07 +0200

    Fixed the route for the submit-request page, added layout

--- app/requester/dashboard/submit-request/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import HealthAssistanceRequestForm from '../components/HealthAssistanceRequestForm';
-
-const Dashboard = () => {
-    return <HealthAssistanceRequestForm />;
-};
-
-export default Dashboard;
---
+++ app/requester/submit-request/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HealthAssistanceRequestForm from './components/HealthAssistanceRequestForm';
+
+const Dashboard = () => {
+  return <HealthAssistanceRequestForm />;
+};
+
+export default Dashboard;
