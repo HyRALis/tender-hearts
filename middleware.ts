3798bd87a05517a2e66edf24fc18f836164a6d20
HyRALis <petar.trajanoski.pt@gmail.com>
2024-06-08 11:22:50 +0200

    Middleware for authentication, protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,43 @@
+import { withAuth } from 'next-auth/middleware';
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest } from 'next/server';
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'mk'],
+export const locales = ['en', 'mk'] as const;
+export const protectedRoutes = ['admin', 'donor', 'requester', 'reviewer'];
 
-  // Used when no locale matches
+const intlMiddleware = createMiddleware({
+  locales,
   defaultLocale: 'en',
+  localeDetection: false,
 });
 
+const authMiddleware = withAuth(
+  function onSuccess(req) {
+    return intlMiddleware(req);
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token != null,
+    },
+    pages: {
+      signIn: '/login',
+    },
+  }
+);
+
+export default function middleware(req: NextRequest) {
+  const excludePattern =
+    '^/(' + locales.join('|') + ')/(' + protectedRoutes.join('|') + ')/?.*?$';
+  const publicPathnameRegex = RegExp(excludePattern, 'i');
+  const isPublicPage = !publicPathnameRegex.test(req.nextUrl.pathname);
+
+  if (isPublicPage) {
+    return intlMiddleware(req);
+  } else {
+    return (authMiddleware as any)(req);
+  }
+}
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(mk|en)/:path*'],
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
