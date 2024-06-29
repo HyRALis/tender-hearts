b47196e3855f44782fdb3e52f68f631fd0e1b591
HyRALis <petar.trajanoski.pt@gmail.com>
2024-06-29 14:14:42 +0200

    Attempt 2 for client side role validation

--- app/_lib/shared/components/role-validation/RoleValidationWrapper.tsx
+++ app/_lib/shared/components/role-validation/RoleValidationWrapper.tsx
@@ -1,19 +1,25 @@
-import React, { FC, ReactNode } from 'react';
+'use client';
+import React, { FC, ReactNode, useEffect } from 'react';
 import { UnauthorizedRedirect } from './UnauthorizedRedirect';
 import { getServerSession } from 'next-auth';
 import { nextAuthOptions } from '@/app/config/nextAuthOptions';
 import { TRole } from '@/app/_lib/types/shared';
-import { getSession } from 'next-auth/react';
+import { getSession, useSession } from 'next-auth/react';
 
 export const RoleValidationWrapper: FC<{
   role: TRole;
   children: ReactNode;
-}> = async ({ role, children }) => {
-  const session = await getSession();
+}> = ({ role, children }) => {
+  const session = useSession();
 
-  if (session?.user.role && session.user.role !== role) {
-    return <UnauthorizedRedirect role={session.user.role} />;
+  useEffect(() => {
+    console.log(session);
+  }, [session]);
+
+  if (session && session.data?.user.role && session.data.user.role !== role) {
+    return <UnauthorizedRedirect role={session.data.user.role} />;
   } else {
     return <>{children}</>;
   }
+  // return <>{children}</>;
 };
