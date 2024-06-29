3caa2b8f2695ae075c833e05b38e42376b6a5827
HyRALis <petar.trajanoski.pt@gmail.com>
2024-06-29 14:09:32 +0200

    made RoleValidationWrapper client component

--- app/_lib/shared/components/role-validation/RoleValidationWrapper.tsx
+++ app/_lib/shared/components/role-validation/RoleValidationWrapper.tsx
@@ -3,12 +3,13 @@ import { UnauthorizedRedirect } from './UnauthorizedRedirect';
 import { getServerSession } from 'next-auth';
 import { nextAuthOptions } from '@/app/config/nextAuthOptions';
 import { TRole } from '@/app/_lib/types/shared';
+import { getSession } from 'next-auth/react';
 
 export const RoleValidationWrapper: FC<{
   role: TRole;
   children: ReactNode;
 }> = async ({ role, children }) => {
-  const session = await getServerSession(nextAuthOptions);
+  const session = await getSession();
 
   if (session?.user.role && session.user.role !== role) {
     return <UnauthorizedRedirect role={session.user.role} />;
